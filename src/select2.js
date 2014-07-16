8523a5bb7aa4c539996ac664492f1dca50a3a45e
Tanner Linsley <tannerlinsley@gmail.com>
2014-07-16 11:01:27 -0600

    Allow Custom ID's from Angular to Select2

--- src/select2.js
+++ src/select2.js
@@ -71,7 +71,14 @@ angular.module('ui.select2', []).value('uiSelect2Config', {}).directive('uiSelec
             angular.forEach(
               angular_data,
               function(value, index) {
-                model.push({'id': value, 'text': value});
+                if (typeof value.text !== 'undefined') {
+                  model.push({
+                    id: value.id,
+                    text: value.text
+                  });
+                } else {
+                  model.push(value);
+                }
               });
           } else {
             model = angular_data;
